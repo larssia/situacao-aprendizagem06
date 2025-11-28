f7a670a8874704f7b3d5dd632f7d22fb667a95af
LARISSA BERNARDI <larissa_bernardi@estudante.sesisenai.org.br>
2025-11-28 12:34:37 -0300

    versão 17

---
+++ index.js
@@ -0,0 +1,7 @@
+function abrirMenu() {
+    document.getElementById("menu-lateral").style.width = "250px";
+}
+
+function fecharMenu() {
+    document.getElementById("menu-lateral").style.width = "0";
+}
